ad228fbf06c40f83bce97466da697d5b43b7a1bc
martiabr <martin.abrandt@outlook.com>
2020-01-12 22:13:05 +0100

    Add simple plane controls

--- terrain-gen/Player.gd
+++ terrain-gen/Player.gd
@@ -1,93 +1,26 @@
 extends KinematicBody
 
-var vel = Vector3()
-const MAX_SPEED = 20
-const ACCEL = 4.5
+onready var movement_guide = $MovementGuide
 
-var dir = Vector3()
-
-const DEACCEL= 16
-const MAX_SLOPE_ANGLE = 40
-
-var camera
-var rotation_helper
-
-var MOUSE_SENSITIVITY = 0.1
+var turn_speed = 50
+var speed = 10
+var rot = Vector3()
 
 func _ready():
-	camera = $Rotation_Helper/Camera
-	rotation_helper = $Rotation_Helper
-	
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
 
 func _physics_process(delta):
 	process_input(delta)
-	process_movement(delta)
+	
+	rotation_degrees = rot
+	translate(-movement_guide.transform.basis.z * speed * delta)
 
 func process_input(delta):
-	dir = Vector3()
-	var cam_xform = camera.get_global_transform()
-	
-	var input_movement_vector = Vector3()
-	
-	if Input.is_action_pressed("movement_forward"):
-	    input_movement_vector.y += 1
-	if Input.is_action_pressed("movement_backward"):
-	    input_movement_vector.y -= 1
-	if Input.is_action_pressed("movement_right"):
-	    input_movement_vector.x += 1
-	if Input.is_action_pressed("movement_left"):
-	    input_movement_vector.x -= 1
 	if Input.is_action_pressed("movement_up"):
-    	input_movement_vector.z += 1
+    	rot.x += turn_speed * delta
 	if Input.is_action_pressed("movement_down"):
-    	input_movement_vector.z -= 1
-	
-	input_movement_vector = input_movement_vector.normalized()
-	
-	# Basis vectors are already normalized.
-	dir += -cam_xform.basis.z * input_movement_vector.y
-	dir += cam_xform.basis.x * input_movement_vector.x
-	dir += cam_xform.basis.y * input_movement_vector.z
-
-	# ----------------------------------
-	
-	# ----------------------------------
-	# Capturing/Freeing the cursor
-	if Input.is_action_just_pressed("ui_cancel"):
-	    if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
-	        Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	    else:
-	        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	# ----------------------------------
-
-func process_movement(delta):
-	dir = dir.normalized()
-	
-	#vel.y += delta * GRAVITY
-	
-	var hvel = vel
-	
-	var target = dir
-	target *= MAX_SPEED
-	
-	var accel
-	if dir.dot(hvel) > 0:
-	    accel = ACCEL
-	else:
-	    accel = DEACCEL
-	
-	hvel = hvel.linear_interpolate(target, accel * delta)
-	vel.x = hvel.x
-	vel.y = hvel.y
-	vel.z = hvel.z
-	vel = move_and_slide(vel, Vector3(0, 1, 0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE))
-
-func _input(event):
-	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-	    rotation_helper.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY * -1))
-	    self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1))
-	
-	    var camera_rot = rotation_helper.rotation_degrees
-	    camera_rot.x = clamp(camera_rot.x, -70, 70)
-	    rotation_helper.rotation_degrees = camera_rot
+    	rot.x -= turn_speed * delta
+	if Input.is_action_pressed("movement_right"):
+		rot.y -= turn_speed * delta
+	if Input.is_action_pressed("movement_left"):
+		rot.y += turn_speed * delta
